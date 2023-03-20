04d1f74139297ef650781c1391583019cabd05e3
Kiera Cox <kiera.cox18@gmail.com>
2023-03-20 16:44:08 -0700

    Outputting names and ratings

---
+++ ratings.py
@@ -0,0 +1,27 @@
+"""Restaurant rating lister."""
+
+def get_restaurant_ratings(file):
+# open the file
+    ratings_file = open(file)
+    
+# empty dictionary to store ratings
+    restaurant_ratings = {}
+
+# need to split each line at ":"]
+    for line in ratings_file:
+        line = line.rstrip().split(":")
+
+# add the lines from the list into a dictionary
+        restaurant_ratings[line[0]] = line[1]
+
+#to sort: try sorted(restaurant_ratings.items()) should gives us the keys as a alphabetically sorted list .
+    sorted_ratings = sorted(restaurant_ratings.items())
+# loops through the sorted ratings tuple to print ratings output
+    for restaurant in sorted_ratings:
+    #assigning variables to each idex of the tuples within the list
+        restaurant_name = restaurant[0]
+        rating = restaurant[1]
+        print(f"{restaurant_name} is rated at {rating}")
+            
+
+
